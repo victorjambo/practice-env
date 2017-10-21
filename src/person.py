d3f1988004c7d354a28ef6da37760ded87a84756
victorjambo <victorjambo@live.com>
2017-10-21 13:13:10 +0300

    Setting up the environments

---
+++ src/person.py
@@ -0,0 +1,21 @@
+"""
+create virtual environment
+virtualenv vn-name-of-env
+
+activate env
+source vn-name-of-env/bin/activate
+
+install pip in that env
+
+add our dependancies to our requirements file
+pip freeze > requirements.txt
+
+to install our env from requirements file
+pip install -r requirements.txt
+"""
+
+
+
+class Person(object):
+    def __init__(self):
+        pass
